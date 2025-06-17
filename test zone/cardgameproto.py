d3652853690a8128cbda6b4da69669c3430af807
gossfunkel <katherineegoss@gmail.com>
2025-06-17 02:43:27 +0100

    more basic card mechanics

--- test zone/cardgameproto.py
+++ test zone/cardgameproto.py
@@ -1,4 +1,5 @@
 from ursina import *
+from queue import Queue
 import sys
 import numpy as np
 
@@ -11,22 +12,45 @@ window.fps_counter.enabled = True
 window.exit_button.enabled = False
 window.color = color.black
 
-def dragme():
-	testbutt.dragging = True
+buttq = Queue(maxsize=10) # hand of cards
+counter = 0 
 
-testbutt = Button(scale=1, color=color.green, origin=[0,0], label="testme", on_click=dragme, dragging=False)
+def spawn():
+	global counter
+	if not buttq.full():
+		counter += 1
+		return buttq.put(Draggable(scale_x=0.3, scale_y=0.4, color=color.yellow, position=(-.7 + counter/5,-.5), text="testme"))
+	else:
+		print("Queue full!")
+		return False
 
-def input(self):
-	if testbutt.dragging:
-		testbutt.origin = mouse.origin
+def despawn():
+	global counter
+	if not buttq.empty():
+		counter -= 1
+		buttq.get().enabled = False # does this actually remove the object from memory?
+	else:
+		print("Queue already empty!")
+		return False
 
-# def update():
-# 	pass
+spawnbutt = Button(scale_x=.4, scale_y=.12, color=color.green, position=(-.75,.4), text="spawn a card", on_click=spawn)
+rmvbutt = Button(scale_x=.4, scale_y=.12, color=color.red, position=(-.75,.24), text="despawn a card", on_click=despawn)
+
+#def input(self):
+#	pass
+
+def update():
+	pass
+	#if buttq.full:
+	#	despawn()
+	#	window.color = color.orange
+	#else:
+	#	window.color = color.black
 
 #if __name__ == "__main__":
 #	sys.exit()
 #else:
 app.run()
 
-#sys.exit()
+sys.exit()
 
