f24f47c8bfef2758d95445f1605350c054aa5c43
gossfunkel <katherineegoss@gmail.com>
2025-06-29 01:35:43 +0100

    more panda3d experiments

--- test zone/pandaTest.py
+++ test zone/pandaTest.py
@@ -1,18 +1,58 @@
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import Shader#
+from panda3d.core import Shader, Geom, GeomNode, GeomTriangles, GeomVertexWriter
+from panda3d.core import GeomVertexFormat, GeomVertexData, TransparencyAttrib
 from math import sin, cos
+
+def createColouredRect(x, z, width, height, colors=None):
+	_format = GeomVertexFormat.getV3c4()
+	vdata   = GeomVertexData('square', _format, Geom.UHDynamic)
+
+	vertex  = GeomVertexWriter(vdata, 'vertex')
+	color   = GeomVertexWriter(vdata, 'color')
+
+	vertex.addData3(x, 0, z)
+	vertex.addData3(x+width, 0, z)
+	vertex.addData3(x+width, 0, z+height)
+	vertex.addData3(x, 0, z+height)
+
+	if colors:
+		if len(colors) < 4:
+			colors = (colors[0],colors[0],colors[0],1.0)
+		color.addData4f(colors)
+		color.addData4f(colors)
+		color.addData4f(colors)
+		color.addData4f(colors)
+	else:
+		color.addData4f(1.0,0.0,0.0,0.0)
+		color.addData4f(0.0,1.0,0.0,0.0)
+		color.addData4f(0.0,0.0,1.0,0.0)
+		color.addData4f(0.0,0.0,0.0,1.0)
+
+	tris = GeomTriangles(Geom.UHDynamic)
+	tris.addVertices(0,1,2)
+	tris.addVertices(2,3,0)
+
+	square = Geom(vdata)
+	square.addPrimitive(tris)
+	return square
 
 class TestBase(ShowBase):
 	def __init__(self):
 		ShowBase.__init__(self)
 		# get nodepath for environment model, then reparent to render node
-		self.scene = self.loader.loadModel("models/environment")
-		self.scene.reparentTo(self.render)
+		#self.scene = self.loader.loadModel("models/environment")
+		#self.scene.reparentTo(self.render)
 		# Apply scale and position transforms on the model.
-		self.scene.setScale(0.25)
-		self.scene.setPos(0, 42, 0)
+		#self.scene.setScale(0.25)
+		#self.scene.setPos(0, 42, 0)
 
 		self.cam.setPos(-5, 0, 3)
+
+		newSquare = createColouredRect(0, 0, 100, 100)
+
+		geomNode = GeomNode('square')
+		geomNode.addGeom(newSquare)
+		self.render.attachNewNode(geomNode)
 
 		# get nodepath for box, then reparent to render node
 		self.sphere = self.loader.loadModel("models/misc/sphere")
@@ -36,6 +76,6 @@ app = TestBase()
 shader = Shader.load(Shader.SL_GLSL,
                      vertex="testVertexShader.vert",
                      fragment="testFragShader.frag")
-app.scene.setShader(shader)
+camera.setShader(shader)
 
 app.run()
