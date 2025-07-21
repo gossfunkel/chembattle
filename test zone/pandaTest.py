bd0e7f68c7d2c801334edb6c93f1805afaddca8c
Gossfunkel <katherineegoss@gmail.com>
2025-07-21 19:57:13 +0100

    200 calculations per tick, over 3.2khz of calculations! Parallels worked in the end. Now seem to be bottlenecked by number of spheres, most likely due to passing each location one by one

--- test zone/pandaTest.py
+++ test zone/pandaTest.py
@@ -2,25 +2,25 @@ from direct.showbase.ShowBase import ShowBase
 from panda3d.core import Shader, Geom, GeomNode, GeomTriangles, GeomVertexWriter
 from panda3d.core import GeomVertexFormat, GeomVertexData, TransparencyAttrib
 from panda3d.core import NodePath, DirectionalLight, PointLight, LightRampAttrib
-from panda3d.core import loadPrcFileData, AsyncTaskManager, AsyncTaskChain, AsyncTask
+from panda3d.core import loadPrcFileData
 from direct.interval.IntervalGlobal import *
+from direct.interval.IntervalManager import ivalMgr
 from direct.filter.CommonFilters import CommonFilters
 from math import sin, cos
 import numpy as np
 
 #from panda3d.core import Thread
 
-loadPrcFileData('', 'win-size 1000 600') 
+loadPrcFileData('', 'win-size 1400 900') 
 loadPrcFileData('', 'show-frame-rate-meter 1')
 loadPrcFileData('', 'hardware-animated-vertices true')
 loadPrcFileData('', 'basic-shaders-only false')
 loadPrcFileData('', 'threading-model Cull/Cull') # creates a different two-thread model: App is on its own thread, 
 										# and Cull and Draw are together on a separate thread. This is most appropriate
-										# when the total amount of time for App in your application is similar to the 
-										# total amount of time for Cull + Draw.
+										# when the total time doing App is similar to the total time for Cull + Draw.
 
 	## how many spheres
-sphereNum = 50
+sphereNum = 100
 
 	### physical values
 chrg = np.array([0.41 for _ in range(sphereNum)])
@@ -33,13 +33,13 @@ kc = 8.9875517923E9*NA*1E30*ech*ech/1E24 # electrostatic const in Daltons, elect
 gravConst = 6.6743e-11 					# G in m3/kg/s
 	## constant deltatime - n.b. for scientific accuracy, we would want this to 
 		# have a resolution of less than 2fs (2e-15). 
-		# The current value is: 20ps (1e-8 secs). 
+		# The current value is: 2ps (2e-9 secs). 
 		# You can't see much happening with much smaller values, but they're realistic. 
-setdt = 0.00000002
+setdt = 0.2E-8
 	## derivatives of motion for calculations
 acc = np.zeros((sphereNum,3))
 vel = np.zeros((sphereNum,3))
-pos = np.array([[(sin(i)*5.)+50.,cos(i)*5.0-150.0,-15.0-i/4] for i in range(sphereNum)])
+pos = np.array([[(sin(i)*2.)+50.,cos(i)*2.0-150.0,-15.0-i/4.] for i in range(sphereNum)])
 #ep = np.array([3.24 for _ in range(sphereNum)])
 #sg = np.array([0.98 for _ in range(sphereNum)])
 ep = 3.24
@@ -163,7 +163,7 @@ class TestBase(ShowBase):
 				# see https://docs.panda3d.org/1.10/python/programming/intervals/index#intervals
 				# and https://docs.panda3d.org/1.10/python/introduction/tutorial/using-intervals-to-move-the-panda
 		self.spheres = np.zeros(sphereNum)
-		self.thetas = np.zeros(sphereNum)
+		#self.thetas = np.zeros(sphereNum)
 		self.sphere = self.loader.loadModel("../sphere")
 		self.sphere.setScale(0.5)
 		self.sphereNodep = NodePath('spheres') # placeholders can be attached to another node to spawn groups of instances
@@ -172,116 +172,87 @@ class TestBase(ShowBase):
 			placeholder.setScale(0.5)
 			placeholder.setPos(pos[i,0],pos[i,1],pos[i,2])
 			self.sphere.instanceTo(placeholder)
-			self.thetas[i] = 0.0
+			#self.thetas[i] = 0.0
 		placeholder = render.attachNewNode("SphereGroup")
 		placeholder.setPos(-50,180,5)
 		self.sphereNodep.instanceTo(placeholder)
 
 		#self.asyncTaskMgr = AsyncTaskManager("asyncTaskMgr").getGlobalPtr()
 		# create a task chain capable of multithreading
-		self.taskMgr.setupTaskChain('physTaskChain', 
-									numThreads=8, 
-									threadPriority=1)
+		#self.taskMgr.setupTaskChain('physTaskChain', 
+		#							numThreads=8, 
+		#							threadPriority=1)
 		# initialise variables and weights for rk4 in scope, each as an array of size sphereNum, populated with 3d vectors
 		self.k = np.array([np.zeros((sphereNum,3)) for _ in range(4)])
 		self.r = pos
 		self.v = vel
 		self.t = 0.0
 		self.rk4step = 0
-		self.tasks = []
-		# generate a task to calculate for each sphere
+		#self.tasks = []
+		# generate a task/interval to calculate for each sphere
+		self.odeParallel = Parallel(name="odeParallel")
 		for i in range(sphereNum):
-			
-			self.tasks.append(taskMgr.doMethodLater(0, self.ode, "sphere_ode", extraArgs=[i], taskChain="physTaskChain", 
-																 sort=0, appendTask=True))
-		# parallelPhys = Parallel(name="parallelPhys")
-		# for i in range(sphereNum):
-		# 	parallelPhys.append(Func(forceCalculation, i, dt)
+			self.odeParallel.append(Func(self.ode, i))
+			#self.tasks.append(taskMgr.doMethodLater(0, self.ode, "sphere_ode", extraArgs=[i], taskChain="physTaskChain", 
+			#													 sort=0, appendTask=True))
 		self.taskMgr.add(self.update, "update", taskChain='default')
 	
-	def ode(self, i, task): 
+	def ode(self, i): 
 		force = -dLJP(self.r,i) + coul(self.r,i) #+ grav(r,i) 								  #!!! CURRENTLY, ADDING GRAVITY HALVES THE FRAMERATE
 		self.k[self.rk4step,i] = np.array([np.transpose(np.transpose(force) / mass[i])]) 	#!!!!!!!!!!!!!!!!! hacky - only works while masses are equal
 		self.v[i] = self.v[i] + self.k[self.rk4step,i] * self.t 		# find r'(t) = v(t) from a = r''(t)
 		self.r[i] = self.r[i] + self.v[i] * self.t 						# find r(t)
-		return task.done	
+		return 1 # task.done	
 
-	async def update(self, task):
+	def update(self, task):
 		global pos, vel
 		
 		#dt = globalClock.getDt()
 		#self.plnp.setPos(180*sin(dt), 100*sin(dt), 200)
 
-		for i in range(4):
-			#print("stepping into rk4 stage " + str(i+1))
-			#currentTsk = self.taskMgr.getCurrentTask()
-			#Task.sequence(taskMgr.getTasks())
-			if (taskMgr.mgr.getActiveTasks().hasTask(self.tasks[sphereNum-1])): # wait for previous round of tasks to finish
-				for tsk in taskMgr.mgr.getActiveTasks():
-					if (tsk.name == "sphere_ode"):
-						#print("awaiting " + str(tsk))
-						await tsk
-			self.r = pos 
-			self.rk4step = i
-			if (i == 0): 
-				self.t = 0
-				self.v = vel
-			elif (i == 1 or i == 2): 
-				self.t = setdt/2. 
-				self.v = vel + self.k[i-1] * setdt/2
-			else: 
-				self.t = setdt
-				self.v = vel + self.k[3] * setdt
+		# run 200 physics calculations per tick
+		for j in range(200):
+			for i in range(4):
+				#print("stepping into rk4 stage " + str(i+1))
+				#currentTsk = self.taskMgr.getCurrentTask()
+				#Task.sequence(taskMgr.getTasks())
+				#if (taskMgr.mgr.getActiveTasks().hasTask(self.tasks[sphereNum-1])): # wait for previous round of tasks to finish
+				#	for tsk in taskMgr.mgr.getActiveTasks():
+				#		if (tsk.name == "sphere_ode"):
+				#			#print("awaiting " + str(tsk))
+				#			await tsk
+				self.r = pos 
+				self.rk4step = i
+				if (i == 0): 
+					self.t = 0.
+					self.v = vel
+				elif (i == 1 or i == 2): 
+					self.t = setdt/2. 
+					self.v = vel + self.k[i-1] * setdt/2
+				else: 
+					self.t = setdt
+					self.v = vel + self.k[3] * setdt
 
-			for tsk in self.taskMgr.getDoLaters(): # run one task per sphere
-				if (tsk.name == "sphere_ode"):
-					#print(tsk)
-					tsk.again
-			#if (i==0):
-			#	#self.taskMgr.waitForTasks()
-			#		# load values from completed tasks into k1? they should probably just save to k1
-			#		#k1[j] = self.asyncTaskMgr.tasks[j].a
-			#		#self.asyncTaskMgr.tasks[j] = ODE(self.asyncTaskMgr.tasks[j].pos, self.asyncTaskMgr.tasks[j].vel + k1*setdt/2, setdt/2) # set up for round 2
-			#elif (i==1):
-			#	k2 = np.empty((sphereNum,3))
-			#	self.asyncTaskMgr.waitForTasks()
-			#	for j in range(sphereNum): # add one task per sphere
-			#		k2[j] = self.asyncTaskMgr.tasks[j].a
-			#		self.asyncTaskMgr.tasks[j] = ODE(self.asyncTaskMgr.tasks[j].pos, self.asyncTaskMgr.tasks[j].vel + k2*setdt/2, setdt/2) # set up for round 3
-			#elif (i==2):
-			#	k3 = np.empty((sphereNum,3))
-			#	self.asyncTaskMgr.waitForTasks()
-			#	for j in range(sphereNum): # add one task per sphere
-			#		k2[j] = self.asyncTaskMgr.tasks[j].a
-			#		self.asyncTaskMgr.tasks[j] = ODE(self.asyncTaskMgr.tasks[j].pos, self.asyncTaskMgr.tasks[j].vel + k3*setdt, setdt) # set up for round 4
-			#elif (i==3):
-			#	k4 = np.empty((sphereNum,3))
-			#	self.asyncTaskMgr.waitForTasks()
-			#	for j in range(sphereNum): # add one task per sphere
-			#		k4[j] = self.asyncTaskMgr.tasks[j].a
-			#		# this is messy - should only need to do this once
-			#		pos = self.asyncTaskMgr.tasks[j].r
-			#		vel = self.asyncTaskMgr.tasks[j].v
-		
-		if (taskMgr.getTasksNamed("sphere_ode") != 0): # wait for previous round of tasks to finish
-				for tsk in taskMgr.getTasksNamed("sphere_ode"):
-					#print("awaiting " + str(tsk))
-					await tsk
+				self.odeParallel.start()
 
-		vel = self.v + setdt/6*(self.k[0] + 2*self.k[1] + 2*self.k[2] + self.k[3])
-		pos = self.r
+				#for tsk in self.taskMgr.getDoLaters(): # run one task per sphere
+				#	if (tsk.name == "sphere_ode"):
+				#		#print(tsk)
+				#		tsk.again
+			# (after rk4, inside physics iterations)
+			#if (taskMgr.getTasksNamed("sphere_ode") != 0): # wait for previous round of tasks to finish
+			#	for tsk in taskMgr.getTasksNamed("sphere_ode"):
+			#		#print("awaiting " + str(tsk))
+			#		await tsk
+			vel = self.v + setdt/6*(self.k[0] + 2*self.k[1] + 2*self.k[2] + self.k[3])
+			pos = self.r
 		#print(pos)
-
+		
 		# # update the positions of the spheres:
 		#self.sphereNodep.getChildren().setPos(pos)
 		for i in range(sphereNum):
 			#print(pos[i])
-			self.sphereNodep.getChild(i).setFluidPos(pos[i,0],pos[i,1],pos[i,2])
-			
-				## dance in a sine-wave pattern:
-			#self.thetas[i] += (i/100 + 2.0) * dt
-			#self.sphereNodep.getChild(i).setPos(i/4 - 50,180,(sin(self.thetas[i])*6)+10) 
-			#self.sphereNodep.getChild(i).setHpr(cos(self.thetas[i]),0,sin(self.thetas[i]))
+			self.sphereNodep.getChild(i).setPos(pos[i,0],pos[i,1],pos[i,2])
 
 		return task.cont
 
